a861a994c6c26c463c835b4256ba4c1cf989e0c9
Kolpashchikov Sergey <SYuKolpashchikov@yandex.ru>
2020-09-17 02:39:26 +0500

    4 проектная работа (2)

--- pages/script.js
+++ pages/script.js
@@ -1,37 +1,30 @@
 let popup = document.querySelector('.popup');
 let open = document.querySelector('.profile__edit-button');
 let close = document.querySelector('.popup__close-button');
-let savebtn = document.querySelector('.popup__button');
+let formElement = document.querySelector('.popup__content');
+let nameInput = formElement.querySelector('.popup__form_input_name');
+let jobInput = formElement.querySelector('.popup__form_input_job');
+let profilename = document.querySelector('.profile__name');
+let profilejob = document.querySelector('.profile__job');
 
 function openpopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profilename.textContent;
+  jobInput.value = profilejob.textContent;
+  
 }
 
 function closepopup() {
   popup.classList.remove('popup_opened');
 }
 
-open.addEventListener('click', openpopup);
-close.addEventListener('click', closepopup);
-savebtn.addEventListener('click', closepopup);
-
-
-let formElement = document.querySelector('.popup__content');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-
-    let nameInput = formElement.querySelector('.popup__form_name');
-    let jobInput = formElement.querySelector('.popup__form_job');
-    
-    nameInput = nameInput.value;
-    jobInput = jobInput.value;
-
-    let profilename = document.querySelector('.profile__name');
-    let profilejob = document.querySelector('.profile__job');
-
-    profilename.textContent = nameInput;
-    profilejob.textContent = jobInput;
+    profilename.textContent = nameInput.value;
+    profilejob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
 }
 
+open.addEventListener('click', openpopup);
+close.addEventListener('click', closepopup);
 formElement.addEventListener('submit', formSubmitHandler);
